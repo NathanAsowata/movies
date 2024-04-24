77979d9a7faf7c66974bf25dd2078c13afb64977
Nathan <nathanasowata@gmail.com>
2024-04-24 23:58:59 +0100

    fix: fallback routing issue

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   disable: false,
-  fallbacks: "/offline",
   workboxOptions: {
     mode: "production",
     disableDevLogs: true,
